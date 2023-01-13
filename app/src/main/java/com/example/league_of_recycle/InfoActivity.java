9f59fdedd7af0faf8a9b5ce223a6b45e93e7c53d
Jaime <jdl0006@alu.medac.es>
2023-01-13 13:04:46 +0100

    FAQ Activity actualizada

--- app/src/main/java/com/example/league_of_recycle/InfoActivity.java
+++ app/src/main/java/com/example/league_of_recycle/InfoActivity.java
@@ -1,13 +1,23 @@
 package com.example.league_of_recycle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.LayoutTransition;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
+
+    TextView detailsText;
+    LinearLayout layout;
 
     ImageButton casa;
     ImageButton mapa;
@@ -20,8 +30,13 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        detailsText = findViewById(R.id.respuesta1);
+        layout = findViewById(R.id.layout1);
+        layout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
         // Casa
         casa = findViewById(R.id.imageButton7);
+
 
         casa.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -69,6 +84,15 @@ public class InfoActivity extends AppCompatActivity {
                 startActivity(info);
             }
         });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void expand(View view){
+        int v = (detailsText.getVisibility() == View.GONE) ? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout, new AutoTransition());
+        detailsText.setVisibility(v);
     }
 
 }
